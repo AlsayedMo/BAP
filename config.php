af780b85c85120622749bb87503b3caac0222a22
Mohamed Alsayed <93587781+AlsayedMo@users.noreply.github.com>
2022-06-10 12:45:35 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'server' => 'localhost',
+    'database' => 'portfoilo',
+    'username' => 'root',
+    'password' => '',
+    'port' => 3306
+];
+return $config;
+
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    require 'function.php';
+    $connection = dbConnect();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEO</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="neon">
+        <!--Circles-->
+        <figure class="circle circle--first"></figure>
+        <figure class="circle circle--second"></figure>
+        <figure class="circle circle--third"></figure>
+        <figure class="circle circle--fourth"></figure>
+        
+        <h1>YO mijn Naam is LEVI </h1>
+        <ul class="cards">
+            <li class="card"></li>
+            <li class="card"></li>
+            <li class="card"></li>
+            <li class="card"></li>
+            <li class="card"></li>
+            <li class="card"></li>
+        </ul>
+    </header>
+</body>
+</html>
